47f9566cef60c45f0b57c032d19d7238f6e88e07
Anton Zhyltsou <antonzhiltsov2000@gmail.com>
2020-06-29 18:37:29 +0200

    Removed secret key

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3-4hzcd^u3=4j1zurjmgv=d&v-b+40fhgn+bc9#a9vl1de7(nv'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
